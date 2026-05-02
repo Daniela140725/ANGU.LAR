4a639daf7dc3631d6e8dc1b6bd33744c1a2f4d1d
Daniela140725 <marquezmurciasara13@gmail.com>
2026-05-02 10:29:29 -0500

    Guia 2 lista de tareas completa

--- mi-primer-app/src/app/app.ts
+++ mi-primer-app/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Perfil } from "./perfil/perfil"; 
+import { ListaTareas } from './lista-tareas/lista-tareas';
 
 @Component({
   selector: 'app-root',
-  imports:[Perfil],
+  standalone: true,
+  imports:[Perfil, ListaTareas],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ mi-primer-app/src/app/tarea-item/tarea-item.ts
@@ -0,0 +1,27 @@
+import { Component, input, output } from '@angular/core';
+import { Tarea } from '../tarea.model';
+
+@Component({
+  selector: 'app-tarea-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './tarea-item.html',
+  styleUrl: './tarea-item.css'
+})
+export class TareaItem {
+
+  // RECIBE datos del padre
+  tarea = input.required<Tarea>();
+
+  // ENVÍA eventos al padre
+  completar = output<number>();
+  eliminar = output<number>();
+
+  alCompletar(): void {
+    this.completar.emit(this.tarea().id);
+  }
+
+  alEliminar(): void {
+    this.eliminar.emit(this.tarea().id);
+  }
+}
